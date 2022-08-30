a0e3d338ee4bdfa28d1ed3a75c4e99e8f2b0722e
Luca <luca@weve-got-it.com>
2022-08-30 14:40:09 +0200

    Initial Commit

---
+++ Project-9-Drawing-5-Animatable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-9-Drawing-5-Animatable
+//
+//  Created by Luca Capriati on 2022/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
